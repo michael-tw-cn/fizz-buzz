d31e389ff997a707fc92543cef15d01c412bb31f
Michael <michaelxmn@163.com>
2020-01-11 21:22:32 +0800

    extract method for conditions

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -2,27 +2,39 @@ package com.thoughtworks;
 
 public class FizzBuzz {
     public String fizzbuzz(int number) {
-        if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
+        if (canModuloBy3(number) && canModuloBy5(number) && canModuloBy7(number)) {
             return "FizzBuzzWhizz";
         }
-        if (number % 3 == 0 && number % 5 == 0) {
+        if (canModuloBy3(number) && canModuloBy5(number)) {
             return "FizzBuzz";
         }
-        if (number % 5 == 0 && number % 7 == 0) {
+        if (canModuloBy5(number) && canModuloBy7(number)) {
             return "BuzzWhizz";
         }
-        if (number % 3 == 0 && number % 7 == 0) {
+        if (canModuloBy3(number) && canModuloBy7(number)) {
             return "FizzWhizz";
         }
-        if (number % 3 == 0) {
+        if (canModuloBy3(number)) {
             return "Fizz";
         }
-        if (number % 5 == 0) {
+        if (canModuloBy5(number)) {
             return "Buzz";
         }
-        if (number % 7 == 0) {
+        if (canModuloBy7(number)) {
             return "Whizz";
         }
         return "1";
+    }
+
+    private boolean canModuloBy7(int number) {
+        return number % 7 == 0;
+    }
+
+    private boolean canModuloBy5(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean canModuloBy3(int number) {
+        return number % 3 == 0;
     }
 }
